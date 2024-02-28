3198ff798f8dc883fa22b7c18d48d9128835fa8e
bomcgoni <bomcgoni@cisco.com>
2024-02-28 16:05:53 -0500

    Readme and Script updates

--- QuickDoc_Manager.js
+++ QuickDoc_Manager.js
@@ -28,20 +28,20 @@ or implied.
 import xapi from 'xapi';
 
 const config = {
-  UserInterface: {                              // Edit the Panel button generated on the Room Navigator
-    Name: 'Docs and Community Spaces',
-    Color: '800000',
-    Icon: 'Language'
+  UserInterface: {
+    Name: 'Docs and Community Spaces',          // AcceptedValues: String || Configure the Name of the Quick Docs Panel
+    Color: '800000',                            // AcceptedValues: String || Configure the Color of the Quick Docs Panel with a hexadecimal color code
+    Icon: 'Language'                            // AcceptedValues: String || Configure the Icon of the Quick Docs Panel
   }
-}
+};
 
 //Edit the list below to add or remove more Web Accessible Docs
 const Site_List = [
   {
-    Name: 'xAPI Developers and Integrations',   // Name visible to the User
-    Url: 'https://eurl.io/#ywxvh25AA',          // URL to the Site
-    Category: 'Webex Spaces',                   // Category of Document. Use this to paginate URLs
-    EnableQR: true
+    Name: 'xAPI Developers and Integrations',   // AcceptedValues: String || Configure the user facing name of the Site
+    Url: 'https://eurl.io/#ywxvh25AA',          // AcceptedValues: String || Configure the URL of the site
+    Category: 'Webex Spaces',                   // AcceptedValues: String || Configure a Category for the Site, leave as '' if a category is unneeded
+    EnableQR: true                              // AcceptedValues: Boolean || If set to true, the QR Code button will appear for this site
   },
   {
     Name: 'AV Integrators Discussion with Cisco',
@@ -61,16 +61,19 @@ const Site_List = [
     Category: '',
     EnableQR: true
   }
-]
+];
+
+const qrDocsPanelId = 'uDocsQR~panel';
 
 async function buildUI() {
-  let pageList = []
+  console.log({ Message: 'Building Userinterface...' })
+  let pageList = [];
   Site_List.forEach((element, index) => {
     if (element.Category == '') {
-      Site_List[index].Category = 'General'
-    }
-    pageList.push(element.Category)
-  })
+      Site_List[index].Category = 'General';
+    };
+    pageList.push(element.Category);
+  });
   pageList = [...new Set(pageList)];
   pageList = pageList.sort((a, b) => {
     if (a === 'General') {
@@ -79,18 +82,19 @@ async function buildUI() {
       return 1; // Move 'General' to the beginning
     } else {
       return a.localeCompare(b); // Sort alphabetically
-    }
+    };
   });
 
-  let pages = {}
+  console.info({ Info: `Page Categories discovered`, Categories: pageList });
 
-  pageList.forEach(element => { pages[element] = {}; pages[element].buttonXML = `` })
+  let pages = {};
+
+  pageList.forEach(element => { pages[element] = {}; pages[element].buttonXML = `` });
 
   pageList.forEach(element => {
-    //pages[element] = {};
-    //pages[element].buttonXML = ``
     Site_List.forEach(el => {
       if (element == el.Category) {
+        console.debug({ Debug: `Adding [${el.Name}] to [${el.Category}] page`, Url: el.Url });
         if (el.EnableQR) {
           pages[element].buttonXML = pages[element].buttonXML + `<Row>
             <Name>${el.Name}</Name>
@@ -106,7 +110,7 @@ async function buildUI() {
               <Type>Button</Type>
               <Options>size=2</Options>
             </Widget>
-          </Row>`
+          </Row>`;
         } else {
           pages[element].buttonXML = pages[element].buttonXML + `<Row>
             <Name>${el.Name}</Name>
@@ -116,24 +120,24 @@ async function buildUI() {
               <Type>Button</Type>
               <Options>size=4</Options>
             </Widget>
-          </Row>`
-        }
-      }
-    })
+          </Row>`;
+        };
+      };
+    });
 
     pages[element].pageXML = `<Page>
       <Name>${element}</Name>
         ${pages[element].buttonXML}
       <PageId>uDocsQR~[Category]</PageId>
       <Options/>
-    </Page>`
-  })
+    </Page>`;
+  });
 
-  let allPages = ``
+  let allPages = ``;
 
   pageList.forEach(element => {
-    allPages = allPages + pages[element].pageXML
-  })
+    allPages = allPages + pages[element].pageXML;
+  });
 
   let xml = `<Extensions>
     <Panel>
@@ -146,36 +150,64 @@ async function buildUI() {
       ${allPages}
     </Panel>
   </Extensions>
-  `
+  `;
 
-  await xapi.Command.UserInterface.Extensions.Panel.Save({ PanelId: 'uDocsQR~panel' }, xml)
+  await xapi.Command.UserInterface.Extensions.Panel.Save({ PanelId: qrDocsPanelId }, xml);
+  console.log({ Message: `Userinterface built, look for a panel titled [${config.UserInterface.Name}] on your touch panel` });
 }
-
-buildUI()
 
 async function openWebView(name = 'Cisco', url = 'https://cisco.com', qrMode = false) {
   if (qrMode) {
-    xapi.Command.UserInterface.WebView.Display({ Title: name + ' QR Code', Url: `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(url)}&size=500x500`, Target: 'Controller', Mode: 'Modal' })
-    console.log({ Message: 'QR Displayed', Name: name, URL: url })
-    return
-  }
+    xapi.Command.UserInterface.WebView.Display({
+      Title: name + ' QR Code',
+      Url: `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(url)}&size=500x500`,
+      Target: 'Controller',
+      Mode: 'Modal'
+    });
+    console.log({ Message: 'QR Displayed', Name: name, URL: url });
+    return;
+  };
 
-  xapi.Command.UserInterface.WebView.Display({ Title: name, Url: url, Target: 'Controller', Mode: 'Modal' })
-  console.log({ Message: 'Website Displayed', Name: name, URL: url })
-  return
+  xapi.Command.UserInterface.WebView.Display({ Title: name, Url: url, Target: 'Controller', Mode: 'Modal' });
+  console.log({ Message: 'Website Displayed', Name: name, URL: url });
+  return;
+};
+
+function delay(ms) { return new Promise(resolve => setTimeout(resolve, ms)) };
+
+async function init() {
+
+  if (Site_List.length == 0) {
+
+    console.error({
+      Error: 'Sites not configured in SiteList',
+      Tip: 'Configure the Sitelist Object at the top of the Macro with at least 1 configured site',
+      Action: 'Disabling this macro in 5 seconds due to unresolvable conflict'
+    });
+
+    await delay(5000)
+    await xapi.Command.UserInterface.Extensions.Panel.Remove({ PanelId: qrDocsPanelId })
+    await xapi.Command.Macros.Macro.Deactivate({ Name: _main_macro_name() });
+    await xapi.Command.Macros.Runtime.Restart();
+
+    return;
+  };
+
+  buildUI();
+
+  xapi.Event.UserInterface.Extensions.Widget.Action.on(event => {
+    if (event.Type == 'released' && event.WidgetId.includes('uDocsQR~')) {
+      let [app, category, name, action, url] = event.WidgetId.split(`~`);
+      switch (action) {
+        case 'OpenSite':
+          openWebView(name, url, false);
+          break;
+        case 'OpenQRCode':
+          openWebView(name, url, true);
+          break;
+      };
+    };
+  });
 }
 
-xapi.Event.UserInterface.Extensions.Widget.Action.on(event => {
-  if (event.Type == 'released' && event.WidgetId.includes('uDocsQR~')) {
-    let [app, category, name, action, url] = event.WidgetId.split(`~`)
-
-    switch (action) {
-      case 'OpenSite':
-        openWebView(name, url, false)
-        break;
-      case 'OpenQRCode':
-        openWebView(name, url, true)
-        break;
-    }
-  }
-})
+init();
